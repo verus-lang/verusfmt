6bd13554d22c632dbafe8b41128ab0e88f4c5062
Jay Bosamiya <jaybosamiya@gmail.com>
2023-07-11 22:41:58 -0400

    Initial sketch for verusfmt

---
+++ src/main.rs
@@ -0,0 +1,106 @@
+#![allow(dead_code, unused_imports)] // TEMPORARY
+
+use std::path::PathBuf;
+
+use anyhow::anyhow;
+use clap::Parser as ClapParser;
+use pest::Parser;
+use pest_derive::Parser;
+use tracing::{debug, error, info, trace};
+
+#[derive(Parser)]
+#[grammar = "verus.pest"]
+pub struct VerusParser;
+
+/// An opinionated formatter for Verus code
+///
+/// Formats code that is inside the `verus!{}` macro. Best utilized in conjunction with rustfmt
+/// which will format everything outside the verus macro.
+#[derive(ClapParser)]
+#[command(version, about)]
+struct Args {
+    /// Run in 'check' mode. Exits with 0 only if the file is formatted correctly.
+    #[arg(long = "check")]
+    check: bool,
+    /// Input file to be formatted
+    file: PathBuf,
+    /// Print debugging output (can be repeated for more detail)
+    #[arg(short = 'd', long = "debug", action = clap::ArgAction::Count)]
+    debug_level: u8,
+}
+
+// XXX: Should we expose this to the user as a configurable option where we pick a default? I think
+// even this should be opinionated, but might be useful to expose this as an unstable option or
+// something.
+const NUMBER_OF_COLUMNS: u32 = 120;
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    tracing_subscriber::fmt()
+        .with_timer(tracing_subscriber::fmt::time::uptime())
+        .with_level(true)
+        .with_target(false)
+        .with_max_level(match args.debug_level {
+            0 => tracing::Level::WARN,
+            1 => tracing::Level::INFO,
+            2 => tracing::Level::DEBUG,
+            _ => tracing::Level::TRACE,
+        })
+        .init();
+
+    if !args.file.is_file() {
+        return Err(anyhow!("{:?} is not a valid file", args.file));
+    }
+
+    let unparsed_file = std::fs::read_to_string(&args.file)?;
+    let parsed_file = VerusParser::parse(Rule::file, &unparsed_file)?
+        .next()
+        .expect("There will be exactly one `file` rule matched in a valid parsed file")
+        .into_inner();
+
+    info!(file = %args.file.display(), "Parsed");
+    trace!(parsed = %parsed_file, "Parsed file");
+
+    let mut formatted_output = String::new();
+
+    for pair in parsed_file {
+        let rule = pair.as_rule();
+        debug!(?rule, "Processing top-level");
+        match rule {
+            Rule::non_verus | Rule::COMMENT => {
+                formatted_output += pair.as_str();
+            }
+            Rule::verus_macro_use => {
+                error!("TODO: format before placing in output");
+                formatted_output += pair.as_str();
+            }
+            Rule::EOI => {
+                // end of input; do nothing
+            }
+            _ => unreachable!("Unexpected rule: {:?}", rule),
+        }
+    }
+
+    if args.check {
+        if unparsed_file == formatted_output {
+            info!("✨Perfectly formatted✨");
+            return Ok(());
+        } else {
+            info!("Found some differences");
+            error!("Input found not to be well formatted");
+            let diff = similar::udiff::unified_diff(
+                similar::Algorithm::Patience,
+                &unparsed_file,
+                &formatted_output,
+                3,
+                Some(("original", "formatted")),
+            );
+            println!("{diff}");
+            return Err(anyhow!("invalid formatting"));
+        }
+    } else {
+        error!("TODO: write output to file");
+        return Err(anyhow!("unimplemented"));
+    }
+}
