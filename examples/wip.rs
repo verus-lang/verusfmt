105f641f45154df3317b062532ee59726201e443
Bryan Parno <parno@cmu.edu>
2023-09-07 16:05:19 -0400

    Tweak the ordering inside expr_inner to defer struct_expr, so that we don't match that when processing `unsafe { ... }`.  Restores the test suite to a working state

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,43 +1,7 @@
 verus! {
 
-pub fn test_function<A, B, C>(a: u32, b: bool, c: LongTypeName) -> u32 {
-    let _ = { a_call() };
+pub fn test_function() -> u32 {
     let _ = unsafe { a_call() };
-    let _ = {
-        a_call();
-        b
-    };
-    unsafe {
-        a_call();
-    };
-    let x = 5;
-    a
-}
-pub fn test() -> bool {
-    let z = {
-        let k = 5;
-        k
-    };
-}
-pub fn test() -> bool {
-    {
-        let y = 7;
-        b
-    }
-}
-pub fn test() -> bool {
-    unsafe {
-        let x = 5;
-        x
-    }
-}
-
-pub fn test_function(x: bool, y: bool) -> u32
-    requires
-        x,
-        y,
-{
-    5
 }
 
 } // verus!
