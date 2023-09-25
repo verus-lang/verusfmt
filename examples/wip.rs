546fbec2d042ffafcb4a514f2f23a786eb744025
Bryan Parno <parno@cmu.edu>
2023-09-24 21:17:21 -0400

    Revert "Adjust handling of `= expr` so that WIP goes through" Breaks too many block-expr examples
    This reverts commit d0a6d7dd4d9e6a238f79002ec3d37059bcc35a5a.

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,7 +1,7 @@
 verus! {
 
 pub fn test_function(x: int, y: int) -> u32 {
-    let very_very_very_very_very_very_long =
+    let very_very_very_very_very_very_long = 
         very_very_very_very_very_very_x + very_very_very_very_y;
     5
 }
