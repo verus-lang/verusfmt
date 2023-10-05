6f4c6a5fa018779a49e91210441652eddbafd43c
Bryan Parno <parno@cmu.edu>
2023-10-05 10:58:13 -0400

    Fix a bug with period strings

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,33 +1,8 @@
 verus! {
 
-enum List<T> {
-    Nil,
-    Cons(T, Box<List<T>>),
-}
-fn len<T>(l: List<T>) -> nat {
-    let x = match l {
-        List::Nil => 0,
-        List::Cons(_, tl) => 1 + len(*tl),
-        List::Cons(_, tl) => {
-            let t = 1 + len(*tl);
-            t
-        },
-    };
-    match foo {
-        foo => bar,
-        a_pattern | another_pattern | yet_another_pattern | a_fourth_pattern => { x },
-        a_very_very_very_very_very_very_pattern
-        | another_very_very_very_very_very_pattern
-        | yet_another_very_very_very_pattern
-        | a_fourth_very_very_very_pattern => { x },
-        a_very_very_very_very_very_very_pattern
-        | another_very_very_very_very_very_pattern
-        | yet_another_very_very_very_pattern
-        | a_fourth_very_very_very_pattern => {
-            let x = something_long_and_complicated();
-            x
-        },
-    }
+fn test_views() {
+    let mut v: Vec<u8> = Vec::new();
+    v.push(10);
 }
 
 } // verus!
