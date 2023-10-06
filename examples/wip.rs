61ac30774b7a811ad54437dc2fd25fc0e29e6c46
Bryan Parno <parno@cmu.edu>
2023-10-06 19:22:10 -0400

    Require semicolon after assert by { ... };

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,24 +1,10 @@
 verus! {
 
-spec fn my_spec_fun() -> int {
-    2
-}
-
-/// exec code cannot directly call proof functions or spec functions.
-/// However, exec code can contain proof blocks (proof { ... }),
-/// which contain proof code.
-/// This proof code can call proof functions and spec functions.
-fn test_my_funs(x: u32, y: u32)
-    requires
-        x < 100,
-        y < 100,
-{
-    // my_proof_fun(x, y); // not allowed in exec code
-    // let u = my_spec_fun(x, y); // not allowed exec code
-    proof {
-        let u = my_spec_fun(x as int, y as int);  // allowed in proof code
-        my_proof_fun(u / 2, y as int);  // allowed in proof code
+fn assert_by_test() {
+    assert(f1(3) > 3) by {
+        reveal(f1);
     }
+    assert(f1(3) > 3);
 }
 
 } // verus!
