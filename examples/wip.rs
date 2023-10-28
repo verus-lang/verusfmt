fef8be26abef8c4abbf88400f8d6e73478b23e27
Bryan Parno <parno@cmu.edu>
2023-10-28 19:43:37 -0400

    Fix identifiers again to allow keywords followed by underscores

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,7 +1,16 @@
 verus! {
 
 fn test_function() {
-    todo!()
+    let (mut stream, _addr) = listener.accept().unwrap();
+    match len_valid_owl_Result(&(*arg.data).as_slice()) {
+        Some(l) => {
+            arg.parsing_outcome = owl_Result_ParsingOutcome::Success;
+        },
+        None => {
+            arg.data = rc_new(vec_u8_from_elem(0, 1));
+            arg.parsing_outcome = owl_Result_ParsingOutcome::Failure;
+        },
+    }
 }
 
 } // verus!
