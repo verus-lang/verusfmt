92b4b98569367fc941f31f31bfa26c3dc0c4a8e9
Bryan Parno <parno@cmu.edu>
2023-12-28 15:25:48 -0500

    Allow semi-colon after an item macro_call

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,8 +1,37 @@
 
 verus! {
 
-fn bits_of_int() {
-    seq![0].add(a) 
-}
+tokenized_state_machine!(Dupe<T> {
+    fields {
+        #[sharding(storage_option)]
+        pub storage: Option<T>,
+
+        #[sharding(constant)]
+        pub val: T,
+    }
+
+    init!{
+        initialize_one(t: T) {
+            // Initialize with a single reader
+            init storage = Option::Some(t);
+            init val = t;
+        }
+    }
+
+    #[invariant]
+    pub fn agreement(&self) -> bool {
+        self.storage == Option::Some(self.val)
+    }
+
+    property!{
+        borrow() {
+            guard storage >= Some(pre.val);
+        }
+    }
+
+     #[inductive(initialize_one)]
+     fn initialize_one_inductive(post: Self, t: T) { }
+});
+
 
 } // verus!
