verus-lang/verusfmt

commit 933f72b6ee9a095bd6ae4f04c0254f18edf7ad04

Better error output, and verusfmt before rustfmt
Details:
* run verusfmt formatting before doing rustfmt; this allows us to give
  more precise error locations in case of parse failures; the two
  formatters should otherwise not interact with each other anyways, so
  the choice of which one first was not particularly an important
  decision anyways
* switch from `anyhow` to `miette` to enable nicer outputs
* start reporting the file name for the parse failure
* produce nicer error output along with context lines

src/main.rs
@@ -1,8 +1,8 @@
 use std::path::PathBuf;
 
-use anyhow::anyhow;
 use clap::Parser as ClapParser;
 use fs_err as fs;
+use miette::{miette, IntoDiagnostic, NamedSource};
 use tracing::{error, info}; // debug, trace
 use verusfmt::{parse_and_format, rustfmt};
 
@@ -26,18 +26,20 @@ struct Args {
     debug_level: u8,
 }
 
-fn format_file(file: &PathBuf, check: bool, verus_only: bool) -> anyhow::Result<()> {
-    let unparsed_file = fs::read_to_string(file)?;
+fn format_file(file: &PathBuf, check: bool, verus_only: bool) -> miette::Result<()> {
+    let unparsed_file = fs::read_to_string(file).into_diagnostic()?;
+
+    let verus_fmted = parse_and_format(&unparsed_file).map_err(|e| {
+        e.with_source_code(NamedSource::new(
+            file.to_string_lossy(),
+            unparsed_file.clone(),
+        ))
+    })?;
+
     let formatted_output = if verus_only {
-        parse_and_format(&unparsed_file)?
+        verus_fmted
     } else {
-        let rustfmt_out = match rustfmt(&unparsed_file) {
-            None => {
-                return Err(anyhow!("rustfmt failed"));
-            }
-            Some(s) => s,
-        };
-        parse_and_format(&rustfmt_out)?
+        rustfmt(&unparsed_file).ok_or(miette!("rustfmt failed"))?
     };
 
     if check {
@@ -55,19 +57,19 @@ fn format_file(file: &PathBuf, check: bool, verus_only: bool) -> anyhow::Result<
                 Some(("original", "formatted")),
             );
             println!("{diff}");
-            return Err(anyhow!("invalid formatting"));
+            return Err(miette!("invalid formatting"));
         }
     } else {
-        fs::write(file, formatted_output)?;
+        fs::write(file, formatted_output).into_diagnostic()?;
         Ok(())
     }
 }
 
 // TODO: Call rustfmt on the code too (maybe include an option to skip it)
-fn main() -> anyhow::Result<()> {
+fn main() -> miette::Result<()> {
     let args = Args::parse();
     if args.files.len() == 0 {
-        return Err(anyhow!("No files specified"));
+        return Err(miette!("No files specified"));
     }
 
     tracing_subscriber::fmt()

Cargo.toml
@@ -5,16 +5,17 @@ edition = "2021"
 autoexamples = false
 
 [dependencies]
-anyhow = "1.0.71"
 clap = { version = "4.3.11", features = ["derive"] }
 fs-err = "2.9.0"
 insta = "1.30.0"
 itertools = "0.11.0"
+miette = { version = "5.10.0", features = ["fancy"] }
 pest = "2.0"
 pest_derive = "2.0"
 pretty = "0.12.1"
 regex = "1.9.6"
 similar = "2.2.1"
+thiserror = "1.0.52"
 tracing = "0.1.37"
 tracing-subscriber = { version = "0.3.17" }
 

src/lib.rs
@@ -1290,11 +1290,86 @@ fn is_multiline_comment(pair: &Pair<Rule>) -> bool {
     matches!(&pair.as_span().as_str()[..2], "/*")
 }
 
-pub fn parse_and_format(s: &str) -> Result<String, pest::error::Error<Rule>> {
+#[derive(thiserror::Error, Debug)]
+pub enum ParseAndFormatError {
+    #[error("Failed to parse")]
+    ParseError { pest_err: pest::error::Error<Rule> },
+}
+
+impl From<pest::error::Error<Rule>> for ParseAndFormatError {
+    fn from(pest_err: pest::error::Error<Rule>) -> Self {
+        Self::ParseError { pest_err }
+    }
+}
+
+impl miette::Diagnostic for ParseAndFormatError {
+    fn help<'a>(&'a self) -> Option<Box<dyn std::fmt::Display + 'a>> {
+        match self {
+            ParseAndFormatError::ParseError { pest_err, .. } => {
+                let mut help = String::new();
+                match &pest_err.variant {
+                    pest::error::ErrorVariant::ParsingError {
+                        positives,
+                        negatives,
+                    } => {
+                        if !positives.is_empty() {
+                            help += "Expected one of: ";
+                            for (i, p) in positives.iter().enumerate() {
+                                if i > 0 {
+                                    help += ", ";
+                                }
+                                help += format!("{:?}", p).as_str();
+                            }
+                        }
+                        if !negatives.is_empty() {
+                            if !help.is_empty() {
+                                help += "; ";
+                            }
+                            help += "Negatives: ";
+                            for (i, n) in negatives.iter().enumerate() {
+                                if i > 0 {
+                                    help += ", ";
+                                }
+                                help += format!("{:?}", n).as_str();
+                            }
+                        }
+                        if help.is_empty() {
+                            return None;
+                        }
+                    }
+                    pest::error::ErrorVariant::CustomError { message } => {
+                        help += message;
+                    }
+                }
+                Some(Box::new(help))
+            }
+        }
+    }
+
+    fn labels(&self) -> Option<Box<dyn Iterator<Item = miette::LabeledSpan> + '_>> {
+        match self {
+            ParseAndFormatError::ParseError { pest_err, .. } => {
+                let (start, len) = match pest_err.location {
+                    pest::error::InputLocation::Pos(start) => (start, 1),
+                    pest::error::InputLocation::Span((start, end)) => (start, end - start),
+                };
+
+                Some(Box::new(std::iter::once(miette::LabeledSpan::new(
+                    Some("here".to_owned()),
+                    start,
+                    len,
+                ))))
+            }
+        }
+    }
+}
+
+pub fn parse_and_format(s: &str) -> miette::Result<String> {
     let ctx = Context {
         inline_comment_lines: find_inline_comment_lines(s),
     };
-    let parsed_file = VerusParser::parse(Rule::file, s)?
+    let parsed_file = VerusParser::parse(Rule::file, s)
+        .map_err(ParseAndFormatError::from)?
         .next()
         .expect("There will be exactly one `file` rule matched in a valid parsed file")
         .into_inner();

tests/syntax-rs-unchanged.rs
@@ -10,5 +10,5 @@ use verusfmt::parse_and_format;
 #[test]
 fn syntax_rs_unchanged() {
     let syntax_rs = include_str!("../examples/syntax.rs").to_owned();
-    assert_eq!(parse_and_format(&syntax_rs), Ok(syntax_rs));
+    assert_eq!(parse_and_format(&syntax_rs).unwrap(), syntax_rs);
 }

commit 99af0b4a8e3daab230b1ac7fe6f5c92198a36dee

Fix a bug introduced in 933f72b which only produced the output of rustfmt (not rustfmt + verusfmt) unless the user invoked --verus-only

src/main.rs
@@ -39,7 +39,7 @@ fn format_file(file: &PathBuf, check: bool, verus_only: bool) -> miette::Result<
     let formatted_output = if verus_only {
         verus_fmted
     } else {
-        rustfmt(&unparsed_file).ok_or(miette!("rustfmt failed"))?
+        rustfmt(&verus_fmted).ok_or(miette!("rustfmt failed"))?
     };
 
     if check {