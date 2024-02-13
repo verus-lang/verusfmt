54bf2948eb110f011d999d3a1f197d1c06b141cf
Bryan Parno <parno@cmu.edu>
2024-02-13 09:01:19 -0500

    Add support for the new spec_fn

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,9 +1,11 @@
 verus! {
 
-pub fn test() {
-    if false {
-    }  // Never going to take that branch
+spec fn map(f: spec_fn(A) -> B) {
+    1
+}
 
+spec fn adder(x: int) -> spec_fn(int) -> int {
+    |y: int| x + y
 }
 
 } // verus!
