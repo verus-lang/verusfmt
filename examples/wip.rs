0a17e24bdba9311815f5d8239302f01845ca347a
Bryan Parno <parno@cmu.edu>
2024-02-14 13:17:50 -0500

    Consider not adding newlines for `#[trigger]` attributes

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,11 +1,7 @@
 verus! {
 
-fn borrow_join<'a>(tracked &'a self) {
-    2
-}
-
-fn borrow_join2<'a>(tracked &'a self, x: u32) {
-    2
+fn commutative() {
+    forall|x, y| #[trigger] r(x, y) == #[trigger] r(y, x)
 }
 
 } // verus!
