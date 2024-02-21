bb0d66c4731a6ac5c6bddf2f4d09057dc5c7e23b
Bryan Parno <parno@cmu.edu>
2024-02-20 19:27:11 -0500

    Don't drop "?" or "~" from type bounds

--- examples/wip.rs
+++ examples/wip.rs
@@ -1,10 +1,5 @@
 verus! {
 
-fn test_single_trigger2() {
-    assume(forall|x: int, y: int|
-        #[trigger]
-        f1(x) < 100 && #[trigger]
-        f1(y) < 100 ==> my_spec_fun(x, y) >= x);
-}
+struct ExManuallyDrop<V: ?Sized>();
 
 } // verus!
