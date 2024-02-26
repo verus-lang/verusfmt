aef0bdef998caad082c8f551dcfdd2c55912c79b
Jay Bosamiya <jaybosamiya@gmail.com>
2024-02-26 15:45:34 -0500

    Improve output of syntax.rs-unchanged test

--- tests/syntax-rs-unchanged.rs
+++ tests/syntax-rs-unchanged.rs
@@ -1,7 +1,5 @@
 //! Testing ../examples/syntax.rs
 
-use verusfmt::parse_and_format;
-
 /// Just an automatic test to make sure that ../examples/syntax.rs is left unchanged by verusfmt.
 ///
 /// This is essentially intended to be a snapshot test, like ./snap-tests.rs, but only as a quick
@@ -9,6 +7,17 @@ use verusfmt::parse_and_format;
 /// `syntax.rs` always in sync with the output that would show up from verusfmt.
 #[test]
 fn syntax_rs_unchanged() {
-    let syntax_rs = include_str!("../examples/syntax.rs").to_owned();
-    assert_eq!(parse_and_format(&syntax_rs).unwrap(), syntax_rs);
+    let original = include_str!("../examples/syntax.rs").to_owned();
+    let formatted = verusfmt::parse_and_format(&original).unwrap();
+    if original != formatted {
+        let diff = similar::udiff::unified_diff(
+            similar::Algorithm::Patience,
+            &original,
+            &formatted,
+            3,
+            Some(("original", "formatted")),
+        );
+        println!("{diff}");
+        panic!("Formatted output does not match");
+    }
 }
