323169d9ed6c9fccdd7db5e8240d93653faf3a78
Jay Bosamiya <jayb@microsoft.com>
2025-04-09 18:26:25 -0700

    Add support for `default_ensures` syntax

--- tests/verus-consistency.rs
+++ tests/verus-consistency.rs
@@ -2985,3 +2985,42 @@ proof fn foo(x: proof_fn(a: u32) -> u64, y: proof_fn[Send](a: u32) -> u64) {}
     } // verus!
     ")
 }
+
+#[test]
+fn verus_default_ensures() {
+    let file = r#"verus!{
+fn foo(i: u32) -> (r: u32) requires 0 <= i < 10 ensures i <= r default_ensures i == r { }
+fn bar(i: u32) -> (r: u32) default_ensures i == r requires 0 <= i < 10 ensures i <= r;
+fn baz(i: bool) default_ensures i {}
+}"#;
+    assert_snapshot!(parse_and_format(file).unwrap(), @r"
+    verus! {
+
+    fn foo(i: u32) -> (r: u32)
+        requires
+            0 <= i < 10,
+        ensures
+            i <= r,
+        default_ensures
+            i == r,
+    {
+    }
+
+    fn bar(i: u32) -> (r: u32)
+        default_ensures
+            i == r,
+        requires
+            0 <= i < 10,
+        ensures
+            i <= r,
+    ;
+
+    fn baz(i: bool)
+        default_ensures
+            i,
+    {
+    }
+
+    } // verus!
+    ")
+}
